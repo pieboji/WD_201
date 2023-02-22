ac61f2e6d128b3e9ba126bf7b78d566748e5298c
pieboji <pnoubissiewilfried@gmail.com>
2023-02-22 20:02:22 +0530

    initial commit

---
+++ http-server/index.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const fs = require('fs')
+
+let homeContent = ''
+let registrationContent = ''
+
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err
+  }
+  homeContent = home
+})
+
+fs.readFile('registration.html', (err, registration) => {
+  if (err) {
+    throw err
+  }
+  registrationContent = registration
+})
+
+http
+  .createServer((request, response) => {
+    const url = request.url
+    response.writeHeader(200, { 'Content-Type': 'text/html' })
+    switch (url) {
+      case '/registration':
+        response.write(registrationContent)
+        response.end()
+        break
+      default:
+        response.write(homeContent)
+        response.end()
+        break
+    }
+  })
+  .listen(3000)
