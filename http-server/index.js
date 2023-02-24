33c5a46562381c4e46ba30353b4c6fd3a09a86a8
pieboji <pnoubissiewilfried@gmail.com>
2023-02-24 12:21:13 +0530

    Modified port minimist

--- http-server/index.js
+++ http-server/index.js
@@ -1,36 +1,56 @@
-const http = require('http')
-const fs = require('fs')
+/* eslint-disable space-infix-ops */
+/* eslint-disable quotes */
+const http = require("http")
+const minimistargs = require("minimist")
+const fs = require("fs")
+let homeContent = ""
+let projectContent = ""
+let registrationContent=""
 
-let homeContent = ''
-let registrationContent = ''
+const argument = minimistargs(process.argv.slice(1), {
+  default: {
+    port: 5000
+  }
+})
 
-fs.readFile('home.html', (err, home) => {
+fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err
   }
   homeContent = home
 })
 
-fs.readFile('registration.html', (err, registration) => {
+fs.readFile("project.html", (err, project) => {
   if (err) {
     throw err
   }
-  registrationContent = registration
+  projectContent = project
 })
 
-http
-  .createServer((request, response) => {
-    const url = request.url
-    response.writeHeader(200, { 'Content-Type': 'text/html' })
-    switch (url) {
-      case '/registration':
-        response.write(registrationContent)
-        response.end()
-        break
-      default:
-        response.write(homeContent)
-        response.end()
-        break
-    }
-  })
-  .listen(3000)
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err
+  }
+  registrationContent=registration
+})
+
+http.createServer((request, response) => {
+  const url = request.url
+  response.writeHeader(200, { "Content-Type": "text/html" })
+  switch (url) {
+    case "/project":
+      response.write(projectContent)
+      response.end()
+      break
+
+    case "/registration":
+      response.write(registrationContent)
+      response.end()
+      break
+
+    default:
+      response.write(homeContent)
+      response.end()
+      break
+  }
+}).listen(argument.port)
